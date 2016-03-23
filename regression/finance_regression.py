856e3c38682c429b4689bece9bb471e692bb0879
juan camilo <juanka@tessi-lab-un.local>
2016-03-23 09:06:02 +0100

    add new reponses

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,9 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+## added for quetion
+# features_list = ["bonus", "long_term_incentive"]
+
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,21 +32,23 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
-
+from sklearn import datasets, linear_model
+reg = linear_model.LinearRegression()
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
 
+reg.fit(feature_train,target_train)
 
-
-
-
-
+print "slope ",reg.coef_
+print "intercept ",reg.intercept_
+print "score on training ",reg.score(feature_train,target_train)
+print "score on test ",reg.score(feature_test,target_test)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
