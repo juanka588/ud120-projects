11aeac8b381395428810cd1b22f658c21b8823e8
juanka <juanka588@hotmail.es>
2016-04-07 10:02:44 +0200

    finish witout project

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -24,8 +24,39 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.cross_validation import train_test_split
+
+### your code goes here
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 
-### your code goes here 
+### it's all yours from here forward!
+
+from sklearn import tree
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+
+print "features in data ",len(features[0])
+clf=tree.DecisionTreeClassifier()
+
+clf.fit(features_train,labels_train)
+
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test,pred)
+pred_score = precision_score(labels_test,pred)
+recall_scor = recall_score(labels_test,pred)
+
+
+print "accuracy ", acc
+
+print "precision score ", pred_score
+print "recall score ", recall_scor
+
+print "poi in test set ",sum(labels_test)
+print "persons in test set ",len(labels_test)
+
+print "real ",labels_test
+print "predicted ",pred
+print "true positives",sum(labels_test*pred)
 
 
